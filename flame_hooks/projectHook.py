99a1f39a824616396ea5495dc40c227c3697a809
Manne Öhrström <manne@shotgunsoftware.com>
2014-09-25 09:18:14 +0100

    Tweaks and improvements. Handling of app restarts for now.

--- flame_hooks/projectHook.py
+++ flame_hooks/projectHook.py
@@ -8,30 +8,34 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-
-# Hook called when the user loads a project in the application.
-# projectName : Name of the loaded project -- String.
-def projectChanged(projectName):
-   pass
-
-
 # Hook called when application is fully initialized
 #
 # projectName: the project that was loaded -- String
-def appInitialized( projectName ):
+def appInitialized(projectName):
     """
     Try to start up the flame engine at this point
     """
     import sgtk
-    import os
+    import os    
     
-    # todo - add exception handling
-    print "Starting up toolkit engine..."
-    engine_name = os.environ.get("TOOLKIT_ENGINE_NAME") 
-    context = sgtk.context.deserialize(os.environ.get("TOOLKIT_CONTEXT"))
-    e = sgtk.platform.start_engine(engine_name, context.sgtk, context)
-    print "toolkit engine running! %s" % e 
+    if sgtk.platform.current_engine():
+        # there is already an engine running.
+        # TODO: later on, allow for switching of engines as projects 
+        #       are being switched. For now, issue a warning
+        
+        # TODO: QT messagebox once we have pyside support
+        print "WARNING: Project switching not supported!" 
     
+    else:
+        # no engine running - so start one!
+        
+        try:
+            engine_name = os.environ.get("TOOLKIT_ENGINE_NAME") 
+            context = sgtk.context.deserialize(os.environ.get("TOOLKIT_CONTEXT"))
+            sgtk.platform.start_engine(engine_name, context.sgtk, context)
+        except Exception, e:
+            # TODO - add pyside messagebox
+            print "ERROR: Could not start Toolkit Engine!"
 
 # Hook called after a project has been saved
 #
@@ -43,12 +47,8 @@ def projectSaved( projectName, saveTime, isAutoSave ):
    pass
 
 
-
-
-
-
-
-
-
-
-
+# Hook called when the user loads a project in the application.
+# projectName : Name of the loaded project -- String.
+def projectChanged(projectName):
+    pass    
+    
