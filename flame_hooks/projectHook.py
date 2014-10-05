05fe0ac2d20bfc7d96e3434bc2e17b330ee127c3
Manne Öhrström <manne@shotgunsoftware.com>
2014-10-05 17:51:35 -0400

    Added QT error messages on project switch.

--- flame_hooks/projectHook.py
+++ flame_hooks/projectHook.py
@@ -8,14 +8,20 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-# Hook called when application is fully initialized
+# 
 #
-# projectName: the project that was loaded -- String
+# 
 def appInitialized(projectName):
     """
-    Try to start up the flame engine at this point
+    Hook called when application is fully initialized.
+    
+    :param projectName: String containing the name of the project that was loaded
     """
+    
+    # attempt to start the flame engine if it hasn't already been started.
+    
     import sgtk
+    import traceback
     import os    
     
     if sgtk.platform.current_engine():
@@ -23,32 +29,29 @@ def appInitialized(projectName):
         # TODO: later on, allow for switching of engines as projects 
         #       are being switched. For now, issue a warning
         
-        # TODO: QT messagebox once we have pyside support
-        print "WARNING: Project switching not supported!" 
+        from PySide import QtGui     
+        QtGui.QMessageBox.warning(None,
+                                  "No project switching!",
+                                  "The Shotgun integration does not currently support project switching.\n"
+                                  "Even if you switch projects, any Shotgun specific configuration will\n"
+                                  "remain connected to the initially loaded project.")
     
     else:
         # no engine running - so start one!
-        
         try:
             engine_name = os.environ.get("TOOLKIT_ENGINE_NAME") 
             context = sgtk.context.deserialize(os.environ.get("TOOLKIT_CONTEXT"))
             sgtk.platform.start_engine(engine_name, context.sgtk, context)
-        except Exception, e:
-            # TODO - add pyside messagebox
-            print "ERROR: Could not start Toolkit Engine!"
+        except:
 
-# Hook called after a project has been saved
-#
-# projectName: the project that was saved -- String
-# saveTime   : time to save the project (in seconds) -- Float
-# isAutoSave : true if save was automatically initiated,
-#              false if user initiated -- Bool
-def projectSaved( projectName, saveTime, isAutoSave ):
-   pass
+            # get error details with std python formatting
+            tb = traceback.format_exc()
+            
+            msg =  "The Shotgun integration failed to start. Please contact support.\n"
+            msg += "The following technical details were reported:\n"
+            msg += tb
 
-
-# Hook called when the user loads a project in the application.
-# projectName : Name of the loaded project -- String.
-def projectChanged(projectName):
-    pass    
-    
+            # pop up QT messagebox
+            from PySide import QtGui
+            QtGui.QMessageBox.critical(None, "Shotgun Integration Failure", msg)
+            
