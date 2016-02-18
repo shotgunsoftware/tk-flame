a3c6035c8e7722a9746917778e70e0c9c36a27f1
Josh Tomlinson <josh.tomlinson@shotgunsoftware.com>
2016-02-18 14:55:03 -0500

    #33062 Adds a call to log a user metric for the DCC version
    Closes #10.

--- python/startup/app_launcher.py
+++ python/startup/app_launcher.py
@@ -102,6 +102,12 @@ def launch_flame(dcc_path, dcc_args):
     cmd_line = "\"%s\" %s %s &" % (dcc_path, app_args, " ".join(dcc_args))
     flame_engine.log_debug("Full command line '%s'" % cmd_line)
     flame_engine.log_debug("-" * 60)
+
+    try:
+        flame_engine.log_user_attribute_metric("Flame version", full_version_str)
+    except:
+        # ignore all errors. ex: using a core that doesn't support metrics
+        pass
     
     return os.system(cmd_line)
     
