46a1474beec6e24bf1009da3fc6b9b6d0d25dd5e
Kerby Geffrard <kerby.geffrard@autodesk.com>
2016-12-20 14:15:38 -0500

    Catch potential AuthenticationCancelled

--- engine.py
+++ engine.py
@@ -959,11 +959,16 @@ class FlameEngine(sgtk.platform.Engine):
             full_cmd = "sudo -g %s -u %s bash -c %s" % (e_group, e_user, pipes.quote(full_cmd))
             self.log_debug("Running root but will send the job as [%s] of the [%s] group" % (e_user, e_group))
 
-        # Make sure that the session is not expired
-        sgtk.get_authenticated_user().refresh_credentials()
+        try:
+            # Make sure that the session is not expired
+            sgtk.get_authenticated_user().refresh_credentials()
+            valid_session = True
+        except sgtk.authentication.AuthenticationCancelled:
+            self.log_debug("User cancelled auth. No backburner job will be created.")
+            valid_session = False
 
-        # kick it off        
-        if os.system(full_cmd) != 0:
+        # kick it off
+        if valid_session and os.system(full_cmd) != 0:
             raise TankError("Shotgun backburner job could not be created. Please see log for details.")
 
 
